8a6d9fbc87b9a08d4b02a1b970d1da08fd8ba497
Ben Liu <63529371+ben4932042@users.noreply.github.com>
2021-12-01 02:41:52 +0800

    refactor: add ticket data class

--- model/order.py
+++ model/order.py
@@ -1,24 +1,8 @@
 import dataclasses
 import datetime
-from pydantic import BaseModel, validator
+from pydantic import BaseModel
 from typing import List
 
-
-class OrderInfo(BaseModel):
-    car_no: int
-    seat_no: int
-
-    @validator('car_no')
-    def car_rule(cls, v):
-        if v not in range(10):
-            raise ValueError("get invalid car number")
-        return v
-
-    @validator('seat_no')
-    def seat_rule(cls, v):
-        if v not in range(50):
-            raise ValueError("get invalid seat number")
-        return v
 
 @dataclasses.dataclass
 class OrderWithTimeStampInfo:
